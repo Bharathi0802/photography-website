9f1c9affc1a3543c7cb02f8dc6d0fcdbdd73a64a
Bharathi Subiksha <bharathisubi2002@gmail.com>
2025-07-07 17:00:26 +0530

    refractoed db.php to use secure config.php

--- db.php
+++ db.php
@@ -3,10 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$host = "sql111.byethost18.com";
-$username = "b18_39337788";
-$password = "demopage1";
-$database = "b18_39337788_photography";
+require_once 'config.php';
 
 $conn = new mysqli($host, $username, $password, $database);
 
